1030b706e1f9bb4bbb7fb85840ff63ccdd7ad5aa
João Victor <joaovictorfernandescastro@gmail.com>
2025-11-06 12:12:10 -0300

    feat: add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from './components/Container'
 import { CountDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 
@@ -18,6 +19,27 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='flex flex-col items-center justify-center gap-6'>
+          <div className='flex flex-col items-center justify-center gap-6'>
+            <DefaultInput type='text' />
+          </div>
+
+          <div className='flex flex-col items-center justify-center gap-6'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='flex flex-col items-center justify-center gap-6'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='flex flex-col items-center justify-center gap-6'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   )
