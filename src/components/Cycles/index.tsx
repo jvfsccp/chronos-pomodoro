e1d6ab68217bc223c503ad761de53c27944e5c08
João Victor <joaovictorfernandescastro@gmail.com>
2025-11-11 13:28:09 -0300

    feat: component cycles added

---
+++ src/components/Cycles/index.tsx
@@ -0,0 +1,18 @@
+export function Cycles() {
+  return (
+    <div className='flex flex-col items-center justify-center gap-6'>
+      <span>Ciclos:</span>
+
+      <div className='flex gap-2'>
+        <span className='w-8 h-8 bg-warning rounded-full'></span>
+        <span className='w-8 h-8 bg-primary rounded-full'></span>
+        <span className='w-8 h-8 bg-warning rounded-full'></span>
+        <span className='w-8 h-8 bg-primary rounded-full'></span>
+        <span className='w-8 h-8 bg-warning rounded-full'></span>
+        <span className='w-8 h-8 bg-primary rounded-full'></span>
+        <span className='w-8 h-8 bg-warning rounded-full'></span>
+        <span className='w-8 h-8 bg-info rounded-full'></span>
+      </div>
+    </div>
+  )
+}
